7347929672bdd575801df305aad8069a90ade561
Tomi Chen <tctree333@users.noreply.github.com>
2021-06-09 17:44:48 -0700

    update dependencies

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,8 @@
-const sveltePreprocess = require('svelte-preprocess');
-// const node = require('@sveltejs/adapter-node');
-const netlify = require('@sveltejs/adapter-netlify');
-const pkg = require('./package.json');
+import sveltePreprocess from 'svelte-preprocess';
+import netlify from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
-module.exports = {
+export default {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: [
@@ -22,12 +20,6 @@ module.exports = {
 		adapter: netlify(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-
-		vite: {
-			ssr: {
-				noExternal: Object.keys(pkg.dependencies || {})
-			}
-		}
+		target: '#svelte'
 	}
 };
